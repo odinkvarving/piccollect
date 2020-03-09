62a310d69158c36fcae80fd7713945c09628454b
Magnus Bredeli <magnbred@stud.ntnu.no>
2020-03-09 12:09:03 +0100

    Added Album class to repositary

---
+++ Picollect - Java/src/sample/Java/Album.java
@@ -0,0 +1,10 @@
+package sample.Java;
+
+import java.util.ArrayList;
+
+public class Album {
+    //An ArrayList with images
+    private String albumName;
+    private ArrayList<Image> images;
+
+}
