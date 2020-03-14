82849e907c9f7c43ed30eda7ba321d01d7c13b24
Magnus Bredeli <magnbred@stud.ntnu.no>
2020-03-14 11:29:26 +0100

    refreshing this shit

--- src/sample/Java/AlbumRegister.java
+++
@@ -1,9 +0,0 @@
-package sample.Java;
-
-import java.util.ArrayList;
-
-public class AlbumRegister {
-    //An ArrayList of albums which contains pictures.
-    private ArrayList<Album> albums;
-    private int albumsSize;
-}
