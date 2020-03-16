d79104a31ff9f5b2ae39e0e29cd9276c99d3971b
Håvard Tysland <55196403+haavardtysland@users.noreply.github.com>
2020-03-16 12:50:05 +0100

    updated FileChooser

--- src/sample/Java/test.java
+++ src/sample/Java/test.java
@@ -8,25 +8,29 @@ import java.util.Collections;
 public class test {
     public static void main(String[] args) throws MetadataException {
 
-        FileChooser f1 = new FileChooser();
+        FileChooser fileChooser = new FileChooser();
+
         ArrayList<String> tags = new ArrayList<>();
         ArrayList<String> tags1 = new ArrayList<>();
         tags.add("picklerick");
         tags1.add("picklerick");
         tags1.add("test");
 
-        Image image = new Image(tags,f1.browseFiles());
-        Image image1 = new Image(tags,f1.browseFiles());
-
+        String path = fileChooser.browseFiles();
+        if(path ==null) {
+            System.out.println("you did not choose an image");
+        }
+        Image image = new Image(tags,path);
+        // image1 = new Image(tags,fileChooser.browseFiles());
 
         ArrayList<Image> images = new ArrayList<Image>();
         images.add(image);
-        images.add(image1);
+        //images.add(image1);
 
         Album album = new Album("album", images);
 
         System.out.println(image.getName());
-        System.out.println(image1.getName());
+        //System.out.println(image1.getName());
         System.out.println(album.findImagesByTag("picklerick"));
         System.out.println(album.findImagesByTag("test"));
     }
