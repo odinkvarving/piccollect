7f86d72c459f38912ae350c038f334f78de720e9
odinkvarving <odinkvarving@users.noreply.github.com>
2020-03-19 02:47:30 +0100

    Finished uploadImageToDatabase method. This method works, but needs to be adjusted to give a unique ID that fits in the Integer-range. Also look at comment above the method for extra information.

--- src/sample/Database.java
+++ src/sample/Database.java
@@ -1,5 +1,6 @@
 package sample;
 
+import com.drew.metadata.MetadataException;
 import com.sun.javafx.iio.ImageMetadata;
 import sample.Java.Image;
 import sample.Java.ImageMetaData;
@@ -9,6 +10,7 @@ import java.sql.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class Database {
 
@@ -21,33 +23,33 @@ public class Database {
 
     public Database() throws SQLException {
         connect = DriverManager.getConnection(url, user, pass);
-        PreparedStatement statement = connect.prepareStatement("Insert into image_table (id, title, path, tags, latitude, longitude, registered)Values(?,?,?,?,?,?,?)");
-        statement.setInt(1, 2);
-        statement.setString(2, "TestTitle");
-        statement.setString(3, "TestPath");
-        statement.setString(4, "TestTags");
-        statement.setString(5, "TestLatitude");
-        statement.setString(6, "TestLongitude");
-        statement.setTimestamp(7, Timestamp.valueOf(LocalDateTime.now()));
+    }
 
-        int a = statement.executeUpdate();
-        if(a > 0) {
-            System.out.println("Row updated.");
+    //Image can not be uploaded if some of the values have already been added to the database earlier. Deleting the row and re-adding the same values does not fix this problem. Look for fix.
+    public void uploadImageToDatabase(Image image, String tags, String title) throws SQLException {
+        if(image != null) {
+            PreparedStatement statement = connect.prepareStatement("INSERT INTO image_table (id, title, path, tags, latitude, longitude, registered)VALUES(?,?,?,?,?,?,?)");
+            statement.setInt(1, 13533);
+            statement.setString(2, title);
+            statement.setString(3, image.getFile().getAbsolutePath());
+            statement.setObject(4, tags);
+            statement.setString(5, Double.toString(image.getMetaData().getGeoDataFromMetadata().getLatitude()));
+            statement.setString(6, Double.toString(image.getMetaData().getGeoDataFromMetadata().getLongitude()));
+            statement.setTimestamp(7, image.getMetaData().getTimeFromMetaData());
+
+            int a = statement.executeUpdate();
+            if (a > 0) {
+                System.out.println("Image added to database. ");
+            }
         }
     }
 
-    public void uploadImageToDatabase(Image image, String tags) {
-        image.getMetaData().getUniqueIdFromMetaData();
-        image.getFile().getAbsolutePath();
-        image.getMetaData().getGeoDataFromMetadata().getLatitude();
-        image.getMetaData().getGeoDataFromMetadata().getLongitude();
-        image.getMetaData().getTimeFromMetaData();
-
-    }
-
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, MetadataException {
+        String tags = "randomTag";
+        ArrayList<String> list = new ArrayList<>();
+        Image image = new Image(list, "C:\\Users\\odink\\OneDrive – NTNU\\Programmering2\\Piccollect\\piccollect\\src\\sample\\testBildeGPS.jpg");
         Database database = new Database();
-
+        database.uploadImageToDatabase(image, tags, "Testing123");
         /*try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connect = DriverManager.getConnection(url, user, pass);
