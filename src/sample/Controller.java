49ed2577787b923380fd9379ca41dfab307332fe
Magnus Bredeli <magnbred@stud.ntnu.no>
2020-03-24 10:12:27 +0100

    deletetd unecessery code in Controller.java

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,12 +6,4 @@ import java.util.Arrays;
 
 public class Controller {
 
-    public static void main(String[] args){
-       File test = new File("C:\\Users\\magnu\\OneDrive\\Skrivebord\\piccollect\\src\\sample\\resources\\testBilde1.jpg");
-       System.out.println(test.getName());
-       String splitTest = test.getName();
-       String[] testFormat = splitTest.split("[.]");
-       System.out.println(Arrays.toString(testFormat));
-       System.out.println(testFormat[testFormat.length - 1]);
-    }
 }
