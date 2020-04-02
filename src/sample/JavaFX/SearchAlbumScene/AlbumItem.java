c25578d5a6c3d79832bb8c6a840b9938ad798227
magnbred <magnbred@stud.ntnu.no>
2020-04-02 13:32:38 +0200

    Added some methods to albumItem class

--- src/sample/JavaFX/SearchAlbumScene/AlbumItem.java
+++ src/sample/JavaFX/SearchAlbumScene/AlbumItem.java
@@ -1,5 +1,6 @@
 package sample.JavaFX.SearchAlbumScene;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
@@ -22,10 +23,22 @@ public class AlbumItem extends Button {
             this.imageView = imageNotFound;
         }
         setText(albumName);
+        createButtonLayout();
+        setImageViewProperties();
     }
 
     private void createButtonLayout(){
         setContentDisplay(ContentDisplay.TOP);
+        setPrefSize(150, 160);
+        setStyle("-fx-background-color: #ffffff");
+        setGraphic(imageView);
+        setPadding(new Insets(20, 20, 20, 20));
+    }
 
+    private void setImageViewProperties(){
+        this.imageView.setFitWidth(150);
+        this.imageView.setFitHeight(150);
+        this.imageView.isPickOnBounds();
+        this.imageView.isPreserveRatio();
     }
 }
